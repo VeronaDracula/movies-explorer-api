a859fe0e001901ee8b35f65aed8acb11bbb07a98
Ekaterina Zenkova <e.i.zenkova@yandex.ru>
2022-01-17 17:57:49 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ const allowedCors = [
   'https://graduationproject.zenkova.nomoredomains.rocks',
   'http://graduationproject.zenkova.nomoredomains.rocks',
   'localhost:3000',
-  'http://localhost:3001',
+  'http://localhost:3000',
 ];
 
 module.exports = (req, res, next) => {
